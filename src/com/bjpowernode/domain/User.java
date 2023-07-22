997db3817272f62ca6a9328755f075a282f3a1cb
liuqing <1613469123@qq.com>
2023-07-22 15:46:16 +0800

    测试第三次提交

--- src/com/bjpowernode/domain/User.java
+++ src/com/bjpowernode/domain/User.java
@@ -2,4 +2,48 @@ package com.bjpowernode.domain;
 
 public class User {
     private String name;
+    private String address;
+    private Integer id;
+
+    public User() {
+    }
+
+    public User(String name, String address, Integer id) {
+        this.name = name;
+        this.address = address;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
